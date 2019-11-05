a2010a6f93cae5f6c24c633e3293d9f99aac594a
caoyonghao <491476941@qq.com>
2019-11-05 15:57:53 +0800

    add chalk

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,9 @@
+const chalk = require('chalk');
 const Koa = require('koa');
 const app = new Koa();
+const log = console.log;
+
 app.use(require("koa-static")(__dirname + "./../dist"));
 
 app.listen(3000);
+log(chalk.green('server start at localhost:3000'));
